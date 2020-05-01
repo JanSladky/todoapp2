13c29dd67f0c19f5fdea03577e77b11bf1f91345
JanSladky <44053032+JanSladky@users.noreply.github.com>
2020-05-01 15:19:52 +0200

    Update add-item.php

--- _inc/add-item.php
+++ _inc/add-item.php
@@ -3,9 +3,22 @@
 	// include
 	require 'config.php';
 
+	//funkce ochrany
+	function sql_ochrana($text)
+	{
+	($text);
+	$text = iconv("UTF-8", "UTF-8//IGNORE", $text);
+	$text = htmlspecialchars($text);
+	$text = chop($text);
+	return $text;
+}
+
+$text = sql_ochrana($_POST["message"]);
+
+
 	// add new stuff
 	$id = $database->insert('items', [
-		'text' => $_POST['message']
+		'text' =>   $text
 	]);
 
 	// success
